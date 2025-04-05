11e0ead73bc40a68bd064d1860e0a568db09034b
rcravens <bob.cravens@gmail.com>
2025-04-05 14:42:55 -0500

    refactored common html into layout files

---
+++ views/layout/_page_foot.view.php
@@ -0,0 +1,5 @@
+</main>
+</div>
+<script src="script/app.js"></script>
+</body>
+</html>
---
+++ views/layout/_page_head.view.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html class="h-full bg-gray-100" lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+	<title>Rate This Pic</title>
+</head>
+<body class="h-full">
+<div class="min-h-full">
